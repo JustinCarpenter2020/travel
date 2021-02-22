a917eec0916fc10f07e13fe6e8a972541f0c3e23
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-22 15:44:59 -0700

    trip service

--- Repositories/TripRepository.cs
+++ Repositories/TripRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using travel.Models;
+
 namespace travel.Repositories
 {
-    public class TripRepository
+  public class TripRepository
+  {
+    internal IEnumerable<Trip> GetAll()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Trip GetById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Trip Create(Trip newTrip)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Trip Edit(Trip original)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(Trip trip)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
