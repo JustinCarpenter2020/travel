a2b81177752c5a149f67741dfb0e973efd4b2263
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-22 15:06:09 -0700

    trip controller

--- Services/TripService.cs
+++ Services/TripService.cs
@@ -1,7 +1,33 @@
+using System;
+using travel.Models;
+
 namespace travel.Services
 {
-    public class TripService
+  public class TripService
+  {
+    internal object GetAll()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object GetById(object id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Create(Trip newTrip)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object EditTrip(Trip updatedTrip)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
