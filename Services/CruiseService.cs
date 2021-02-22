01848383e30ba212e8e7cebf77462348428a98c2
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-22 15:29:32 -0700

    controllers done

--- Services/CruiseService.cs
+++ Services/CruiseService.cs
@@ -1,7 +1,33 @@
+using System;
+using travel.Models;
+
 namespace travel.Services
 {
-    public class CruiseService
+  public class CruiseService
+  {
+    internal object GetOne(int id)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object Create(Cruise newCruise)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Cruise updatedCruise)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetAll()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
